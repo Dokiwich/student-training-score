b7b9914606a26dc6e777f5c8101729473d8f901d
Dokiwich <nonamejta@gmail.com>
2026-06-16 09:42:59 +0700

    refactor: remove deprecated and unused utility modules

--- packages/database/tmp_check2.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('./src/generated/client');
-const p = new PrismaClient();
-p.$queryRawUnsafe('SELECT * FROM criteria LIMIT 1')
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => p.$disconnect());
