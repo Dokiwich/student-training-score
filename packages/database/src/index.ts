66e301bddc5f122f0820b754af8d871d1bd1e90d
Dokiwich <nonamejta@gmail.com>
2026-07-02 21:32:17 +0700

    feat: initialize Prisma schema and update Docker build stages to include generated client

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from './generated/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export { prisma };
-export * from './generated/client'; 
+export * from '@prisma/client'; 
 
 // NẾU TRONG FILE INDEX.D.TS NÓ TÊN LÀ scoring_sheets_status, HÃY DÙNG DÒNG NÀY:
-export { scoring_sheets_status as WorkflowStatus } from './generated/client';
+export { scoring_sheets_status as WorkflowStatus } from '@prisma/client';
 
 // CÒN NẾU NÓ TÊN LÀ WorkflowStatus, THÌ CHỈ CẦN DÙNG DÒNG NÀY:
 // export { WorkflowStatus } from './generated/client';
